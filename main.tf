97270ebe5915f6bb15a085830d842a4324427dfd
onifadesa <onifade.abayomi@outlook.com>
2023-04-12 11:23:05 +0100

    added security group for webserver access from everywhere

--- main.tf
+++ main.tf
@@ -1,22 +1,34 @@
 #Define the provider
 provider "aws" {
-    region = "us-east-2"
-  }
+  region = "us-east-2"
+}
 
 #create a VM
 resource "aws_instance" "webserver" {
-    ami = "ami-00eeedc4036573771"
-    instance_type = "t2.micro"
+  ami                    = "ami-00eeedc4036573771"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance_sg.id]
 
-    user_data = <<-EOF
-                #!/bin/bash
-                echo "Welcome To a New World" > index.html
-                nohup busybox httpd -f -p 8080 &
-                EOF
-    user_data_replace_on_change = true
-    tags = {
-      "Name" = "Terraform-Webserver"
-    } 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Welcome To a New World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  user_data_replace_on_change = true
+  tags = {
+    "Name" = "Terraform-Webserver"
+  }
 }
 
 #Create SG for webserver access from anywhere
+resource "aws_security_group" "instance_sg" {
+  name = "terraform-weserver-sg"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
