0b104650d179ad6e39ce18845f4bc0e32012f50c
onifadesa <onifade.abayomi@outlook.com>
2023-04-12 10:58:51 +0100

    adding user_data to create a sample webserver

--- main.tf
+++ main.tf
@@ -8,7 +8,15 @@ resource "aws_instance" "webserver" {
     ami = "ami-00eeedc4036573771"
     instance_type = "t2.micro"
 
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Welcome To a New World" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+    user_data_replace_on_change = true
     tags = {
       "Name" = "Terraform-Webserver"
     } 
 }
+
+#Create SG for webserver access from anywhere
